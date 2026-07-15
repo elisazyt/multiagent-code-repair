800e0ce9c8c311ab1fc2ce6b088dc5ec1d5bf253
elisazhangyt <elisaz.ca@gmail.com>
2026-07-14 23:47:50 -0700

    stop tracking shell script for now

--- main.sh
+++
@@ -1,4 +0,0 @@
-# user needs to define paths, NUM_PATCHING_ATTEMPTS, project name, bug id, and bug files/locations
-# optionally allow user to specify bm25/rag configs
-# also ensure api key is set
-# assumes docker container is built
